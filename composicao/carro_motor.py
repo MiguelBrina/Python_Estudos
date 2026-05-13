bc546329da9bc7270de56111575b1dfaa50c951e
Miguel Brina Dos Santos <miguel.brinaa28@gmail.com>
2026-05-13 16:11:17 -0300

    refactor: transferindo "Exercicio-006.py" para pasta composicao
    Funcionalidades: implementa classe Veiculo que é composta pelo atributo motor do tipo Motor , ou  '''seja composição'''

---
+++ composicao/carro_motor.py
@@ -0,0 +1,27 @@
+class Veiculo:
+    def __init__(self, nome, modelo, marca):
+        self.nome = nome
+        self.modelo = modelo
+        self.marca = marca
+        
+class Motor:
+def ligar(self):
+print("motor ligado") #função de ligar o motor
+    def desligar(self):
+        print("motor desligado") #função de desligar o motor
+
+class Carro(Veiculo):
+    def __init__(self, nome, modelo, marca, motor):
+        super().__init__(nome, modelo, marca)
+        self.motor = motor
+        
+    def ligar_carro(self):
+        self.motor.ligar()
+        
+    def desligar_carro(self):
+        self.motor.desligar()
+
+m = Motor()
+c = Carro("miguel", "onix", "chevrolet", m)
+c.ligar_carro()
+c.desligar_carro()
